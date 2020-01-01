408811d160e01ec7a053edd319169fd9e8968c7f
moofie <406110076@qq.com>
2020-01-01 20:27:42 +0800

    m

--- demos/service_worker_notification/sw.js
+++ demos/service_worker_notification/sw.js
@@ -1,3 +1,36 @@
 self.addEventListener('notificationclick', function (event) {
-  console.log('notification clicked!');
+  var messageId = event.notification.data;
+
+  event.notification.close();
+  // 通过设置的 actions 来做适当的响应
+  if (event.action === 'like') {
+  }
+  clients.openWindow("https://insistandinsist.github.io");
 }, false);
+
+
+// 安装阶段跳过等待，直接进入 active
+self.addEventListener('install', function (event) {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    Promise.all([
+
+      // 更新客户端
+      self.clients.claim(),
+
+      // 清理旧版本
+      caches.keys().then(function (cacheList) {
+        return Promise.all(
+          cacheList.map(function (cacheName) {
+            if (cacheName !== 'my-test-cache-v1') {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    ])
+  );
+});
